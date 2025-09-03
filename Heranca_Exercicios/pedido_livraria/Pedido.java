c531679797cd1721002336e3e5dd655872c96032
Romário Vieira <romario.sistemas@gmail.com>
2025-09-03 00:42:25 -0300

    Pedido livraria

---
+++ Heranca_Exercicios/pedido_livraria/Pedido.java
@@ -0,0 +1,34 @@
+public class Pedido {
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.setPercentualDesconto(percentualDesconto);
+        this.setItens(itens);
+    }
+
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public void setPercentualDesconto(double percentualDesconto) {
+        this.percentualDesconto = percentualDesconto;
+    }
+
+    public ItemPedido[] getItens() {
+        return itens;
+    }
+
+    public void setItens(ItemPedido[] itens) {
+        this.itens = itens;
+    }
+
+    public double calcularTotal() {
+        double total = 0.0;
+        for (ItemPedido item : itens) {
+            total += item.getProduto().obterPrecoLiquido() * item.getQuantidade();
+        }
+        double desconto = total * (this.getPercentualDesconto() / 100);
+        return total - desconto;
+    }
+}
