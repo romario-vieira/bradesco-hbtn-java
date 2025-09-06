f30fc25cbffeb9e09944ffa897d652cb2a39e4ac
Romário Vieira <romario.sistemas@gmail.com>
2025-09-06 19:46:34 -0300

    Correcao

--- 0x03/livraria/Livro.java
+++ 0x03/livraria/Livro.java
@@ -6,7 +6,7 @@ public class Livro {
     private String autor;
     private double preco;
 
-    public Livro(String titulo, String autor, double preco) {
+    public Livro(String titulo, String autor, double preco) throws LivroInvalidoException, AutorInvalidoException {
         this.setTitulo(titulo);
         this.setAutor(autor);
         this.setPreco(preco);
@@ -16,10 +16,11 @@ public class Livro {
         return titulo;
     }
 
-    public void setTitulo(String titulo) {
+    public void setTitulo(String titulo) throws LivroInvalidoException {
         if (titulo.isEmpty() || titulo.length() < 3) {
             throw new LivroInvalidoException("Titulo de livro invalido");
         }
+
         this.titulo = titulo;
     }
 
@@ -27,10 +28,11 @@ public class Livro {
         return autor;
     }
 
-    public void setAutor(String autor) {
+    public void setAutor(String autor) throws AutorInvalidoException {
         if (autor.isEmpty() || autor.split("\\s+").length < 2) {
             throw new AutorInvalidoException("Nome de autor invalido");
         }
+
         this.autor = autor;
     }
 
@@ -38,12 +40,12 @@ public class Livro {
         return preco;
     }
 
-    public void setPreco(double preco) {
+    public void setPreco(double preco) throws LivroInvalidoException {
         if (preco <= 0) {
             throw new LivroInvalidoException("Preco de livro invalido");
         }
+
         this.preco = preco;
     }
 
-    
 }
