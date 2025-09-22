4fb433d9321ce0da215a8e7bf54e787f5c3f4ddf
Romário Vieira <romario.sistemas@gmail.com>
2025-09-22 00:17:40 -0300

    List

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+    private List<PedidoCookie> cookies;
+
+    public Pedido() {
+        cookies = new ArrayList<>();
+    }
+
+    public void adicionarPedidoCookie(PedidoCookie pedidoCookie) {
+        cookies.add(pedidoCookie);
+    }
+
+    public int obterTotalCaixas() {
+        int total = 0;
+        for (PedidoCookie pedidoCookie : cookies) {
+            total += pedidoCookie.getQuantidadeCaixas();
+        }
+        return total;
+    }
+
+    public int removerSabor(String sabor) {
+    int totalRemovido = 0;
+    List<PedidoCookie> paraRemover = new ArrayList<>();
+    for (PedidoCookie pedidoCookie : cookies) {
+        if (pedidoCookie.getSabor().equals(sabor)) {
+            totalRemovido += pedidoCookie.getQuantidadeCaixas();
+            paraRemover.add(pedidoCookie);
+        }
+    }
+    cookies.removeAll(paraRemover);
+    return totalRemovido;
+}
+}
