86e062ce2926caeea027197fbe5221d4acca0d7a
Romário Vieira <romario.sistemas@gmail.com>
2025-09-21 23:02:54 -0300

    generics armazem

---
+++ Generics_Exercises/armazem/Armazem.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Armazem<T> implements Armazenavel<T> {
+    private Map<String, T> inventario;
+
+    public Armazem() {
+        this.inventario = new HashMap<>();
+    }
+
+    @Override
+    public void adicionarAoInventario(String nome, T valor) {
+        inventario.put(nome, valor);
+    }
+
+    @Override
+    public T obterDoInventario(String nome) {
+        return inventario.get(nome);
+    }
+    
+}
