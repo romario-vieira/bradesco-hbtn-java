f4661243f700a5e1940e636a7b48a9222057bf77
Romário Vieira <romario.sistemas@gmail.com>
2025-09-30 19:37:33 -0300

    Streams collectors

---
+++ Streams_Collectors_Optional/livros/Consulta.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
+                .toList();
+    }
+}
