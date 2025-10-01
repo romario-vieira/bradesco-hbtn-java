0da0e3a6b75d84ec1a19daa7d073a4b0998cb616
Romário Vieira <romario.sistemas@gmail.com>
2025-10-01 00:08:03 -0300

    Pedido

---
+++ Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
@@ -0,0 +1,31 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
+                .toList();
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream().sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        return produtos.stream()
+                .filter(produto -> produto.getPreco() >= precoMinimo)
+                .toList();
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        return pedidos.stream()
+                .filter(pedido -> pedido.getProdutos().stream()
+                        .anyMatch(produto -> produto.getCategoria() == CategoriaProduto.ELETRONICO))
+                .toList();
+
+    }
+}
