01ead6e84d4a1b2ef6360238d08b21e65c0538fc
Romário Vieira <romario.sistemas@gmail.com>
2025-09-30 23:43:10 -0300

    Maior preço

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,16 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
+                .toList();
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream().sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .findFirst()
+                .orElse(null);
+    }
+}
