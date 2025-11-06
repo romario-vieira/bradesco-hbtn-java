d966bad5c4563da4b7385cf545276afd52e99197
Romário Vieira <romario.sistemas@gmail.com>
2025-11-05 21:48:39 -0300

    login

---
+++ log/0/VerificacaoLogin.java
@@ -0,0 +1,40 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class VerificacaoLogin {
+
+
+    // Inicialização do logger
+    private static final Logger logger = LoggerFactory.getLogger(VerificacaoLogin.class);
+
+
+    public static void main(String[] args) {
+        // Simulação de entradas do usuário
+        String usuarioCorreto = "admin";
+        String senhaCorreta = "12345";
+
+
+        // Tentativas de login
+        realizarLogin("admin", "12345", usuarioCorreto, senhaCorreta); // Login correto
+        realizarLogin("admin", "senhaErrada", usuarioCorreto, senhaCorreta); // Senha incorreta
+        realizarLogin("usuarioDesconhecido", "12345", usuarioCorreto, senhaCorreta); // Usuário desconhecido
+    }
+
+    public static void realizarLogin(String usuario, String senha, String usuarioCorreto, String senhaCorreta) {
+        // implemente aqui o log
+        logger.info("Tentativa de login com o usuário: {}", usuario);
+        if (usuario.equals(usuarioCorreto) && senha.equals(senhaCorreta)) {
+            logger.info("Login bem-sucedido para o usuário: {}", usuario);
+            return;
+        }
+        if (!usuario.equals(usuarioCorreto)) {
+            logger.warn("Usuário {} não encontrado!", usuario);
+            return;
+        }
+        if (!senha.equals(senhaCorreta)) {
+            logger.error("Senha incorreta para o usuário: '{}'", usuario);
+            return;
+        }
+    }
+}
